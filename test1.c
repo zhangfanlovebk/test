8727b4d6088d61aaa2aef9193563dc93c91b5728
Your Name <you@example.com>
2016-11-14 18:00:29 +0800

    100到200间的素数

--- test1.c
+++ test1.c
@@ -1,6 +1,40 @@
+#define CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main ()
+//{
+//	printf("hello world!\n");
+//	return 0;
+//}
+
+//1. 打印100~200 之间的素数
+
 #include <stdio.h>
-int main ()
+
+#include <math.h>
+
+int main()
+
 {
-	printf("hello world!\n");
-	return 0;
+
+    int i,n,k;
+
+    for(n=100;n<201;n++)
+
+    {
+
+       k=sqrt(n);
+
+    for(i=2;i<=k;i++)
+
+           if(n%i==0)break;
+
+       if(i>k)
+
+           printf("%d\n",n);
+
+    }
+
+    return 0;
+
 }
