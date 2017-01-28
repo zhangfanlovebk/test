5752c0e071a2a190be20d374b8e00b43b2a3d326
Your Name <you@example.com>
2017-01-28 16:12:39 +0800

    大年初一测试

--- test1.c
+++ test1.c
@@ -2356,6 +2356,31 @@ int main()
 #include<stdio.h>
 #include<assert.h>
 
+int my_strcmp(const char* str1,const char* str2)
+{
+	assert(str1);
+	assert(str2);
+	while(*str1 != '\0' && *str2 != '\0'){
+		if(*str1 == *str2){
+			str1++;
+			str2++;
+		}
+		if(*str1 > *str2){
+			return 1;
+		}
+		if(*str1 < *str2){
+			return -1;
+		}
+	}
+	if(*str1 == '\0' && *str2 != '\0'){
+		return -1;
+	}
+	if(*str1 != '\0' && *str2 == '\0'){
+		return 1;
+	}
+	return 0;
+}
+
 char* my_strcpy(char* dst, const char* src)
 {
 	char* ret = dst;
@@ -2372,7 +2397,9 @@ int main()
 {
 	char dst[50];
 	const char* msg = "hello world";
+	const char* msg1 = "hello bit";
 	my_strcpy(dst,msg);
 	printf("%s\n",dst);
+	printf("%d\n",(my_strcmp(msg,msg1)));
 	return 0;
 }
